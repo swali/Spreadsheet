8842b727c5db187940f46afd678ccc43fac4c5a9
Sandeep Wali <swali86@gmail.com>
2023-04-19 01:03:57 -0700

    remove row.css import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import "./styles/cell.css";
-import "./styles/row.css";
 import App from "./App";
 import store from './redux/store';
 import { Provider } from 'react-redux';
